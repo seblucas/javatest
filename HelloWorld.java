60a6bca6c18668cad672032d8d630b78f624102d
Sébastien Lucas <sebastien@slucas.fr>
2019-09-20 12:58:34 +0200

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+// Définition de la classe
+
+public class HelloWorld {
+
+  // Définition de la fonction main
+
+  public static void main(String[] arg){
+
+    // Affichage du commentaire Hello world dans la fenêtre de commande
+
+    System.out.println("Hello world");
+
+  }
+
+}
